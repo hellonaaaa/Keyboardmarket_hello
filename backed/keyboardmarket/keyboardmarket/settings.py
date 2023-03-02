3f4161aa88b49eb32fda773f59a58300bf73af08
hellonaaaa <114495866+hellonaaaa@users.noreply.github.com>
2023-03-03 06:22:29 +0800

    Update settings.py

--- backed/keyboardmarket/keyboardmarket/settings.py
+++ backed/keyboardmarket/keyboardmarket/settings.py
@@ -168,5 +168,5 @@ EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
-EMAIL_HOST_USER = 'tui8855@gmail.com'
-EMAIL_HOST_PASSWORD = 'pyauzxbloybndhrc'
+EMAIL_HOST_USER = ''
+EMAIL_HOST_PASSWORD = ''
